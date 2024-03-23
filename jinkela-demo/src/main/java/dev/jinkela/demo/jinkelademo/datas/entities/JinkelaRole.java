36a38912b1b914e7eaba68a26926881411574604
dfz <sneakydog@yeah.net>
2024-03-24 00:30:04 +0800

    1

--- jinkela-demo/src/main/java/dev/jinkela/demo/jinkelademo/datas/entities/JinkelaRole.java
+++ jinkela-demo/src/main/java/dev/jinkela/demo/jinkelademo/datas/entities/JinkelaRole.java
@@ -1,14 +1,10 @@
 package dev.jinkela.demo.jinkelademo.datas.entities;
 
-import java.time.LocalDateTime;
-
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.relational.core.mapping.Table;
 
 import dev.jinkela.demo.jinkelademo.datas.AuditMetadata;
-import dev.jinkela.demo.jinkelademo.datas.converters.Enabled;
 import lombok.Getter;
 import lombok.Setter;
 
