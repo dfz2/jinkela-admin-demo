4072981cd5c1f3d3c52c75479a67d70647aebd16
dfz <sneakydog@yeah.net>
2024-03-25 22:37:56 +0800

    1

--- jinkela-demo/src/main/java/dev/jinkela/demo/jinkelademo/controller/JinkelaOpenApiController.java
+++ jinkela-demo/src/main/java/dev/jinkela/demo/jinkelademo/controller/JinkelaOpenApiController.java
@@ -12,7 +12,6 @@ import dev.jinkela.demo.jinkelademo.utils.RSAUtil;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RestController
 @RequestMapping("/jinkela/open")
 public class JinkelaOpenApiController {
